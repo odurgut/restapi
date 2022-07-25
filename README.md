ebeb6becdd59c3a8745be659a420060e9466f22c
odurgut <ogzhansari@gmail.com>
2022-07-25 12:22:13 +0300

    Add: Post section to frontend

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # restapi
 
 First Commit
+
+docker run --name postapidb --env POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+Note: To stop you just need to run docker stop postapidb and to remove it docker rm postapidb
--- frontend/index.js
+++ frontend/index.js
@@ -4,10 +4,16 @@ titleBox = document.getElementById("title");
 contentBox = document.getElementById("content");
 authorBox = document.getElementById("author");
 
+postButton = document.getElementById("buttonPost");
+title2Box = document.getElementById("title2");
+content2Box = document.getElementById("content2");
+author2Box = document.getElementById("author2");
+
 eventListeners();
 
 function eventListeners() {
     getButton.addEventListener("click", getData);
+    postButton.addEventListener("click", postData);
 }
 
 function getData() {
@@ -19,9 +25,26 @@ function getData() {
         });
 }
 
+function postData() {
+    const post = {
+        title: title2Box.value,
+        content: content2Box.value,
+        author: author2Box.value,
+    };
+
+    fetch("http://localhost:9000/api/posts", {
+        method: "POST",
+        body: JSON.stringify(post),
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+        });
+}
+
 function updateUI(jsonData) {
-    idBox.textContent = jsonData[0].id;
-    titleBox.textContent = jsonData[0].title;
-    contentBox.textContent = jsonData[0].content;
-    authorBox.textContent = jsonData[0].author;
+    idBox.textContent = jsonData[jsonData.length - 1].id;
+    titleBox.textContent = jsonData[jsonData.length - 1].title;
+    contentBox.textContent = jsonData[jsonData.length - 1].content;
+    authorBox.textContent = jsonData[jsonData.length - 1].author;
 }
